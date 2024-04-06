b4bb6a8ca9f02880b5003abd8ddde3617b5c302d
scuddi <157764402+scuddi@users.noreply.github.com>
2024-04-06 10:04:10 +0200

    final version of text based battle, future changes will be to play around with classes and oop

--- character.py
+++ character.py
@@ -1,4 +1,5 @@
 from weapons import fists
+from health_bar import HealthBar
 
 class Character:
     def __init__(self, name: str, health: int):
@@ -11,13 +12,15 @@ class Character:
     def attack(self, target):
         target.health -= self.weapons.damage
         target.health = max(target.health, 0)
+        target.health_bar.update()
         print(f"{self.name} dealt {self.weapons.damage} damage to {target.name} with {self.weapons.name}")
 
 class Hero(Character):
     def __init__(self, name: str, health: int):
         super().__init__(name=name, health = health)
 
-        self.default_weapon = self.weapon
+        self.default_weapon = self.weapons
+        self.health_bar = HealthBar(self, color="purple")
 
     def equip(self, weapons):
         self.weapons = weapons
@@ -25,10 +28,13 @@ class Hero(Character):
 
     def drop(self):
         self.weapons = self.default_weapon
+        print(f"{self.name} dropped {self.weapons.name}")
 
 
 class Enemy(Character):
     def __init__(self, name: str, health: int, weapons):
         super().__init__(name=name, health = health)
         self.weapons = weapons
+
+        self.health_bar = HealthBar(self, color = "red")
 
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from character import Hero, Enemy
-from weapons import short_bow
+from weapons import short_bow, iron_sword
 
 hero = Hero(name = "Hero", health = 100)
+hero.equip(iron_sword)
 enemy = Enemy(name = "Enemy", health = 100, weapons = short_bow)
 
 while True:
     hero.attack(enemy)
     enemy.attack(hero)
 
-    print(f"Health of {hero.name}: {hero.health}")
-    print(f"Healt of {enemy.name}: {enemy.health}")
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
 
+    print(f"Health of {hero.name}: {hero.health}")
+    print(f"Health of {enemy.name}: {enemy.health}")
+
+    hero.drop()
     input()
